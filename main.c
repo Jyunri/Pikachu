1593908954fd8879cf70177e82490a48af849424
Jyunri <nekojimy@gmail.com>
2017-03-30 12:14:49 -0300

    pikachu drawing

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <GL/glut.h>
 #include <math.h>
+#include <unistd.h>
+
+#define PI M_PI
 
 typedef struct
 {
@@ -26,7 +29,7 @@ typedef struct
 Fila *f;
 GLint N = 3;  //numero de coordenadas do rato
 GLfloat X1 = -.1, Y1 = 0, X2 = .1, Y2 = 0, X3 = 0 , Y3 = .1;
-GLfloat cx[3], cy[3];   //centro do rato
+GLfloat cx, cy;   //centro do rato
 GLfloat ox[3], oy[3];   //origem do rato
 GLfloat rx[3], ry[3];
 GLfloat novo_qx, novo_qy;
@@ -35,10 +38,14 @@ GLfloat dx, dy;
 GLfloat w = 500, h = 500, r = .0, g = .0, b = .0;
 GLint count = -1;
 GLfloat fator_de_escala = 1.2;
+GLfloat angle;
+
+GLfloat corpo_x = 0.12, corpo_y = 0.17;
+float tmp_x, tmp_y;
 
 float toRadian(float grau)
 {
-    return grau*3.14/180;
+    return grau*M_PI/180;
 }
 
 void SpecialKeys (int key, int x, int y)
@@ -63,14 +70,129 @@ void desenha()
     glClearColor(1, 1, 1, 0);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    //desenha Rato
-    glColor3f(r,g,b);
+    //desenha Triangulo teste
+//    glColor3f(255,255,0);
+//    glLineWidth(5.0);
+//
+//    glBegin(GL_TRIANGLES);
+//    for(int i = 0; i < N; i++)
+//    {
+//        glVertex2f(rx[i], ry[i]);
+//    }
+//    glEnd();
+
+    //desenha corpo
+    float angulo;
+    glColor3f(255,255,0);
     glLineWidth(5.0);
-    glBegin(GL_TRIANGLES);
-    for(int i = 0; i < N; i++)
+    glBegin(GL_POLYGON);
+    for (int i=0; i<=50; i++)
     {
-        glVertex2f(rx[i], ry[i]);
+        angulo= 2 * PI * i / 50.0;
+        glVertex3f(cx+corpo_x*cos(angulo), cy+corpo_y*sin(angulo),0);
     }
+    glEnd();
+
+    //desenha nariz
+    glColor3f(0,0,0);
+    glBegin(GL_POLYGON);
+    for (int i=0; i<=50; i++)
+    {
+        angulo= 2 * PI * i / 50.0;
+        glVertex3f(cx+0.008*cos(angulo), cy +0.008*sin(angulo)+0.14,0);
+    }
+    glEnd();
+
+    //desenha olho esquerdo
+    glColor3f(0,0,0);
+    glBegin(GL_POLYGON);
+    for (int i=0; i<=50; i++)
+    {
+        angulo= 2 * PI * i / 50.0;
+        glVertex3f(cx+0.015*cos(angulo)+0.04, cy +0.015*sin(angulo)+0.1,0);
+    }
+    glEnd();
+
+    //desenha olho direito
+    glColor3f(0,0,0);
+    glBegin(GL_POLYGON);
+    for (int i=0; i<=50; i++)
+    {
+        angulo= 2 * PI * i / 50.0;
+        glVertex3f(cx+0.015*cos(angulo)-0.04, cy +0.015*sin(angulo)+0.1,0);
+    }
+    glEnd();
+
+
+    //desenha bochecha esquerda
+    glColor3f(1,0,0);
+    glBegin(GL_POLYGON);
+    for (int i=0; i<=50; i++)
+    {
+        angulo= 2 * PI * i / 50.0;
+        glVertex3f(cx+0.015*cos(angulo)+0.07, cy +0.015*sin(angulo)+0.12,0);
+    }
+    glEnd();
+
+    //desenha bochecha direita
+    glColor3f(1,0,0);
+    glBegin(GL_POLYGON);
+    for (int i=0; i<=50; i++)
+    {
+        angulo= 2 * PI * i / 50.0;
+        glVertex3f(cx+0.015*cos(angulo)-0.07, cy +0.015*sin(angulo)+0.12,0);
+    }
+    glEnd();
+
+    //desenha orelha direita
+    glColor3f(0,0,0);
+    glPointSize(3);
+    glBegin(GL_TRIANGLES);
+        glVertex2f(-.15+cx,-.1+cy);
+        glVertex2f(-.1+cx,.05+cy);
+        glVertex2f(-.11+cx,-.05+cy);
+    glEnd();
+
+    //desenha orelha esquerda
+    glColor3f(0,0,0);
+    glPointSize(3);
+    glBegin(GL_TRIANGLES);
+        glVertex2f(.15+cx,-.1+cy);
+        glVertex2f(.1+cx,.05+cy);
+        glVertex2f(.11+cx,-.05+cy);
+    glEnd();
+
+    //desenha listra 1
+    glColor3f(.8,.6,0);
+    glPointSize(3);
+    glBegin(GL_POLYGON);
+        glVertex2f(.07+cx,.0+cy);
+        glVertex2f(-.07+cx,0+cy);
+        glVertex2f(0+cx,-.02+cy);
+    glEnd();
+
+    //desenha listra 2
+    glColor3f(.8,.6,0);
+    glPointSize(3);
+    glBegin(GL_POLYGON);
+        glVertex2f(.07+cx,-.05+cy);
+        glVertex2f(-.07+cx,-.05+cy);
+        glVertex2f(0+cx,-.07+cy);
+    glEnd();
+
+    //desenha rabo
+    glColor3f(255,255,0);
+    glPointSize(3);
+    glBegin(GL_POLYGON);
+        glVertex2f(-.04+cx,-.16+cy);
+        glVertex2f(-.07+cx,-.19+cy);
+        glVertex2f(-0.025+cx,-.22+cy);
+        glVertex2f(-.07+cx,-.26+cy);
+
+        glVertex2f(.0+cx,-.26+cy);
+        glVertex2f(0.025+cx,-.22+cy);
+        glVertex2f(.0+cx,-.19+cy);
+        glVertex2f(.04+cx,-.16+cy);
     glEnd();
 
     //desenha Queijo
@@ -104,14 +226,16 @@ void moveRato()
     if(removeNo(f))
     {
         printf("Movendo (%d,%d)\n",dx,dy);
+        //rodaRato(); //roda de acordo com a posicao do queijo
+        cx+= dx;
+        cy+= dy;
+
+
         for(int i = 0; i < N; i ++)
         {
             rx[i]+= dx;
             ry[i]+= dy;
-            cx[i]+= dx;
-            cy[i]+= dy;
             glutPostRedisplay();
-
         }
 
     }
@@ -119,14 +243,29 @@ void moveRato()
 
 void rodaRato()
 {
+    //todo definir o angulo certo
+    GLfloat m = (ry[3]-cy)/(rx[3]-cx);    //coeficiente angular do rato
+    GLfloat n = (novo_qy-cy)/(novo_qx-cx);
+    angle = atan(abs((m-n)/(1+(m*n))));
+    printf("m:%f n:%f, angulo: %f\n",m,n,angle);
+
+    angle = toRadian(90);
+
+    /*rotaciona triangulo
     for(int i = 0; i < N; i ++)
     {
         float x = rx[i], y = ry[i];
         //printf("p%d: (%f,%f), c%d:(%f,%f)\n",i,rx[i],ry[i],i,cx[i],cy[i]);
-        rx[i] =cx[i] + ((x-cx[i]) * 0.7 - (y-cy[i]) * 0.7); //+ (rx[i]-ox[i]);
-        ry[i] =cy[i] + ((x-cx[i]) * 0.7 + (y-cy[i]) * 0.7); //+ (ry[i]-oy[i]);
+        rx[i] =cx + ((x-cx) * cos(angle) - (y-cy) * sin(angle)); //+ (rx[i]-ox[i]);
+        ry[i] =cy + ((x-cx) * sin(angle) + (y-cy) * cos(angle)); //+ (ry[i]-oy[i]);
         //printf("pnovo%d: (%f,%f)\n\n",i,rx[i],ry[i]);
     }
+    */
+
+    float x = corpo_x, y = corpo_y;
+    //printf("p%d: (%f,%f), c%d:(%f,%f)\n",i,rx[i],ry[i],i,cx[i],cy[i]);
+    corpo_x = cx + ((x-cx) * cos(angle) - (y-cy) * sin(angle)); //+ (rx[i]-ox[i]);
+    corpo_y = cy + ((x-cx) * sin(angle) + (y-cy) * cos(angle)); //+ (ry[i]-oy[i]);
 
 
     //translada o rato para o ponto original
@@ -143,19 +282,35 @@ void escalaRato (unsigned char key, int x, int y)
     case '+':
         for(int i = 0; i<N; i++)
         {
-            rx[i] = (rx[i]-cx[i])* fator_de_escala + cx[i];
-            ry[i] = (ry[i]-cy[i])* fator_de_escala + cy[i];
+            rx[i] = (rx[i]-cx)* fator_de_escala + cx;
+            ry[i] = (ry[i]-cy)* fator_de_escala + cy;
         }
+        corpo_x = (corpo_x-cx)*fator_de_escala + cx;
+        corpo_y = (corpo_y-cy)*fator_de_escala + cy;
         break;
     case '-':
         for(int i = 0; i<N; i++)
         {
-            rx[i] = (rx[i]-cx[i])*1/fator_de_escala + cx[i];
-            ry[i] = (ry[i]-cy[i])*1/fator_de_escala + cy[i];
+            rx[i] = (rx[i]-cx)*1/fator_de_escala + cx;
+            ry[i] = (ry[i]-cy)*1/fator_de_escala + cy;
         }
+        corpo_x = (corpo_x-cx)*1/fator_de_escala + cx;
+        corpo_y = (corpo_y-cy)*1/fator_de_escala + cy;
         break;
 
-    //teste para o rato correr atras do queijo apenas quando ele teclar
+    //teste: move para origem
+    case 'o':
+        tmp_x = cx;
+        tmp_y = cy;
+        cx = 0;
+        cy = 0;
+        corpo_x = (corpo_x-cx)*fator_de_escala;
+        corpo_y = (corpo_y-cy)*fator_de_escala;
+        cx = tmp_x;
+        cy = tmp_y;
+
+
+    //teste: faz o rato correr atras do queijo apenas quando ele teclar
     case 'm':
         moveRato();
         break;
@@ -180,7 +335,7 @@ void posicionaQueijo (int botao, int estado, int x, int y)
             insereNo(f);
             exibeFila(f);
 
-            rodaRato();
+            //rodaRato();
 
             //TODO - tem que fazer o rato mover aos poucos
             //moveRato(); //leva o rato ate o queijo criado
@@ -286,9 +441,7 @@ int main(int argc, char *argv[ ] )
     ultimo_qy = 0;
 
     //centro do rato
-    cx[0] = 0, cy[0] = 0;
-    cx[1] = 0, cy[1] = 0;
-    cx[2] = 0, cy[2] = 0;
+    cx = 0, cy = 0;
 
 
     //origem para o rato
